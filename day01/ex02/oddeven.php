4c923175fb8ba93175a2471ce4b48a4593690807
Erik Mende <emende@student.hive.fi>
2022-04-13 11:01:07 +0300

    ex02 now works CTRL-D

--- day01/ex02/oddeven.php
+++ day01/ex02/oddeven.php
@@ -1,20 +1,21 @@
 #!/usr/bin/php
 <?PHP
 
-
-$f = fopen( 'php://stdin', 'r' );
-while ($line = fgets( $f ) && !feof($f))
+$input = fopen('php://stdin', 'r');
+echo "Enter a number: ";
+while ($line = fgets($input))
 {
-	echo "Enter a number: ";
-	$f = rtrim(fgets(STDIN));
-	if (feof($input))
-		return;
-	if (!is_numeric($input))
-		print("'$input' is not a number\n");
-	else if ($input % 2 == 0)
-		print("The number $input is even\n");
+	if ($line == NULL)
+		break;
+	$line = trim($line);
+	if (!is_numeric($line))
+		print("'$line' is not a number\n");
+	else if ($line % 2 == 0)
+		print("The number $line is even\n");
 	else
-		print("The number $input is odd\n");
+		print("The number $line is odd\n");
+	echo "Enter a number: ";
 }
+fclose($input);
 
 ?>
