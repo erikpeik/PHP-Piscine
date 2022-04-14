4f697bd7b825b32c863438b09ccd4d9036b01832
Erik Mende <emende@student.hive.fi>
2022-04-14 17:34:16 +0300

    one_more_thing.php done

--- day02/ex01/one_more_time.php
+++ day02/ex01/one_more_time.php
@@ -1,27 +1,55 @@
 #!/usr/bin/php
 <?PHP
 
-function ft_split($str)
+function	panic($str, $int)
 {
-	$str = trim($str);
-	$parts = preg_split("/\s+/", $str);
-	$parts = array_map('trim', $parts);
-	$parts = array_filter($parts);
-	sort($parts);
-	return ($parts);
+	echo $str;
+	exit ($int);
+}
+
+function	check_month($month)
+{
+	$month = lcfirst($month);
+	$months = array("janvier", "février", "mars", "avril", "mai", "juin",
+		"juillet", "août", "septembre", "octobre", "novembre", "décembre");
+	$value = array_search($month, $months);
+	if ($value === false)
+		return (null);
+	return ($value + 1);
+}
+
+function	check_day_of_the_week($day)
+{
+	$day = lcfirst($day);
+	$doft = array("lundi", "mardi", "mercredi",
+		"jeudi", "vendredi", "samedi", "dimanche");
+	$value = array_search($day, $doft);
+	if ($value === false)
+		return (false);
+	return ($value + 1);
 }
 
 function	one_more_time($argc, $argv)
 {
-	if ($argc > 1)
+	if ($argc == 2)
 	{
-		$splits = ft_split($argv[1]);
+		date_default_timezone_set("Europe/Paris");
+		$splits = preg_split("/ /", $argv[1]);
 		if (count($splits) != 5)
-		{
-			echo "Wrong Format\n";
-			exit (-1);
-		}
-		print_r($splits);
+			panic("Wrong Format\n", 1);
+		$month = check_month($splits[2]);
+		if ($month === null || check_day_of_the_week($splits[0]) === false)
+			panic("Wrong Format\n", 2);
+		if (!preg_match('/^\d{4}$/', $splits[3]))
+			panic("Wrong Format\n", 3);
+		if (!preg_match("/^([01]\d|2\d):[0-5]\d:[0-5]\d$/", $splits[4]))
+			panic("Wrong Format\n", 4);
+		if (checkdate($month, $splits[1], $splits[3]) === false)
+			panic("Wrong Format\n", 5);
+		$timestamp = $month."/".$splits[1]."/".$splits[3]." ".$splits[4];
+		if (date('w', strtotime($timestamp)) != check_day_of_the_week($splits[0]))
+			panic("Wrong Format\n", 6);
+		echo strtotime($timestamp), "\n";
 	}
 }
 
