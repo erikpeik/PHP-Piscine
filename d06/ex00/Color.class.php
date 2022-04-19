51571844bd4b658a3e2af033654308db46a42f81
Erik Mende <emende@student.hive.fi>
2022-04-19 22:03:34 +0300

    Put everyfile to executable in d05

--- d06/ex00/Color.class.php
+++ d06/ex00/Color.class.php
@@ -1,29 +1,34 @@
-<?PHP
+<?php
 
 Class Color {
 	public	$red;
 	public	$green;
 	public	$blue;
-	public static $verbose = false;
+	public static $verbose = true;
 
-	public function	__consturct() {
-		echo "Constructor called".PHP_EOL;
+	public function __toString() {
+		return sprintf("Color( red: %3d, green: %3d, blue: %3d )", $this->red, $this->green, $this->blue);
+	}
+
+	public function	__construct(array $color) {
+		if (isset($color['rgb']))
+		{
+			$this->red = (int)$color['rgb'] >> 16 & 0xFF;
+			$this->green = (int)$color['rgb'] >> 8 & 0xFF;
+			$this->blue = (int)$color['rgb'] & 0xFF;
+		}
+		else if (isset($))
+		if (self::$verbose)
+			echo $this." constructed.".PHP_EOL;
 		return ;
 	}
 
-	public function __toString() {
-		return sprintf()
-	}
-	function get_rgb() {
-		return ("rpg(".$this->$red.
-		", ".$this->$green.")".PHP_EOL);
-	}
 	function __destruct() {
-		echo "Constructor called".PHP_EOL;
+		if (self::$verbose)
+			echo $this." destructed.".PHP_EOL;
 		return ;
 	}
 }
 
-
-$split = new Color("404040");
+$teset = new Color(array('rgb' => 0x30b165));
 ?>
