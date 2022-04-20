f2a009a6f34736a1e9c9b5dd5ff5e4e75ddc7f7a
Erik Mende <emende@student.hive.fi>
2022-04-20 22:48:01 +0300

    Started doing d07

---
+++ d07/ex02/Targaryen.class.php
@@ -0,0 +1,14 @@
+<?PHP
+
+class Targaryen {
+	public function resistsFire() {
+		return False;
+	}
+
+	public function	getBurned() {
+		if ($this->resistsFire() )
+			return "emerges naked but unharmed";
+		return "burns alive";
+	}
+}
+?>
---
+++ d07/ex03/House.class.php
@@ -0,0 +1,13 @@
+<?PHP
+
+abstract class House {
+	abstract function	getHouseName();
+	abstract function	getHouseMotto();
+	abstract function	getHouseSeat();
+
+	public function	introduce() {
+		print('House '.$this->getHouseName().' of '.$this->getHouseSeat().' : "'.$this->getHouseMotto().'"'.PHP_EOL);
+	}
+}
+
+?>
