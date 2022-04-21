9ce37a4c30c44679098d6c2e59b7d903ab6d609d
Erik Mende <emende@student.hive.fi>
2022-04-21 19:11:32 +0300

    Added Jaime if he don't that person it will ask who is it

--- d07/ex04/Jaime.class.php
+++ d07/ex04/Jaime.class.php
@@ -4,10 +4,12 @@ Class Jaime {
 	public function	sleepWith($person) {
 		if ($person instanceof Tyrion)
 			echo "Not even if I'm drunk !".PHP_EOL;
-		if ($person instanceof Sansa)
+		else if ($person instanceof Sansa)
 			echo "Let's do this.".PHP_EOL;
-		if ($person instanceof Cersei)
+		else if ($person instanceof Cersei)
 			echo "With pleasure, but only in a tower in Winterfell, then.".PHP_EOL;
+		else
+			echo "Who is that?".PHP_EOL;
 	}
 }
 
