ae848ce4a178c2c433c93929fb1842d9feb3ec1b
Erik Mende <emende@student.hive.fi>
2022-04-23 11:50:53 +0300

    to-do list works with cookies now

--- d09/ex02/todo.js
+++ d09/ex02/todo.js
@@ -1,15 +1,46 @@
-function	add_element() {
-	var input = prompt("New TO-DO", "");
-	if (input == null) {
-		return false;
+function	setCookie(name, value, days) {
+	const d = new Date();
+	d.setTime(d.getTime() + (days * 24 * 60 * 60 * 1000));
+	let expires = "expires=" + d.toUTCString();
+	document.cookie = name + "=" + value + ";"+ expires + ";path=/";
+}
+
+function	removeCookie(name, value) {
+	document.cookie = name + "=" + value
+		+ ";expires=Thu, 01 Jan 1970 00:00:01 GMT" + ";path=/";
+}
+
+window.onload = function() {
+	var cookies = document.cookie.split(';');
+	for (var i = 0; i < cookies.length; i++) {
+		var name = cookies[i].substring(0, cookies[i].indexOf('='));
+		var value = cookies[i].substring(cookies[i].indexOf('=') + 1);
+		add_element(name, value);
 	}
+}
+
+function	add_element(date_id, input) {
+
 	var ft_list = document.getElementById('ft_list');
 	var element = document.createElement('div');
 	element.appendChild(document.createTextNode(input));
 	ft_list.prepend(element);
-	document.cookie = input + '=' + input;
 	element.addEventListener('click', function remove_it() {
-		ft_list.removeChild(element);
-	return true;
+		if (window.confirm("Do you really want to remove this Element?")) {
+			ft_list.removeChild(element);
+			removeCookie(date_id, input);
+		}
 	});
+
+}
+
+function new_prompt() {
+	var input = prompt("New TO-DO", "");
+	if (input == null || input == '') {
+		return false;
+	} else {
+		date_id = Date.now();
+		add_element(date_id, input);
+		setCookie(date_id, input, 1);
+	}
 }
