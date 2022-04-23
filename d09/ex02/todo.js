17ad774e7c6960e2814e8c26646b40ad096c1efd
Erik Mende <emende@student.hive.fi>
2022-04-23 13:59:47 +0300

    encode & decode

--- d09/ex02/todo.js
+++ d09/ex02/todo.js
@@ -2,7 +2,7 @@ function	setCookie(name, value, days) {
 	const d = new Date();
 	d.setTime(d.getTime() + (days * 24 * 60 * 60 * 1000));
 	let expires = "expires=" + d.toUTCString();
-	document.cookie = name + "=" + value + ";"+ expires + ";path=/";
+	document.cookie = name + "=" + encodeURIComponent(value) + ";"+ expires + ";path=/";
 }
 
 function	removeCookie(name, value) {
@@ -25,7 +25,7 @@ function	add_element(date_id, input) {
 	var ft_list = document.getElementById('ft_list');
 	var element = document.createElement('div');
 	element.setAttribute("class", "element");
-	element.appendChild(document.createTextNode(input));
+	element.appendChild(document.createTextNode(decodeURIComponent(input)));
 	element.addEventListener('click', function remove_it() {
 		if (window.confirm("Do you really want to remove this Element?")) {
 			ft_list.removeChild(element);
